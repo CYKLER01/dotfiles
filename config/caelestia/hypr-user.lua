f4eb83ae5e7248b238b70a3e871dc0bb0feb2004
CYKLER01 <chris.clyt@gmail.com>
2026-07-17 17:26:10 +0100

    keybinds

--- config/caelestia/hypr-user.lua
+++ config/caelestia/hypr-user.lua
@@ -1,6 +1,13 @@
+# remove mouse accel
+
 hl.config({
-	input = {
-		accel_profile = "flat",
-		force_no_accel = true,
-	},
+    input = {
+        accel_profile = "flat",
+        force_no_accel = true,
+    },
 })
+
+# Fuzzel keybinds
+hl.bind("SUPER + CTRL + S", hl.dsp.exec_cmd("ssh-menu.sh"))
+hl.bind("SUPER + SPACE", hl.dsp.exec_cmd("power-menu.sh"))
+hl.bind("SUPER + T", hl.dsp.exec_cmd("todo.sh"))
