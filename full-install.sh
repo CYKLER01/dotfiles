0e2f09b6761ac62d31467e72387506fb4cc66468
CYKLER01 <chris.clyt@gmail.com>
2026-07-20 20:16:20 +0100

    fixed full install script

--- full-install.sh
+++ full-install.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
-# ~/.local/bin/bootstrap.sh
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Get the absolute path of the directory where full-install.sh lives
+DOTFILES_ROOT="$(cd "$(dirname "$(realpath "$0")")" && pwd)"
 
 echo "Starting system setup..."
 
-./scripts/installation/pacman.sh
-./scripts/installation/appimages.sh
-./scripts/installation/lazyvim-install.sh
-./scripts/installation/install-bash.sh
-./scripts/installation/install-fuzzel.sh
-./scripts/installation/install-config.sh
-./scripts/installation/wallpapers.sh
+"$DOTFILES_ROOT/scripts/installation/pacman.sh"
+"$DOTFILES_ROOT/scripts/installation/appimages.sh"
+"$DOTFILES_ROOT/scripts/installation/lazyvim-install.sh"
+"$DOTFILES_ROOT/scripts/installation/install-bash.sh"
+"$DOTFILES_ROOT/scripts/installation/install-fuzzel.sh"
+"$DOTFILES_ROOT/scripts/installation/install-config.sh"
+"$DOTFILES_ROOT/scripts/installation/wallpapers.sh"
 
 echo "Bootstrap complete!"
