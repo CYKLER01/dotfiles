e1711a21f56b47ea7e5a15527ebada571f84371d
CYKLER01 <chris.clyt@gmail.com>
2026-07-20 21:01:04 +0100

    added more packages

--- scripts/installation/pacman.sh
+++ scripts/installation/pacman.sh
@@ -4,8 +4,8 @@ sudo pacman -Syu --noconfirm
 
 # Install packages
 sudo pacman -S --noconfirm \
-  hyprland kitty fish neovim git ufw \
-  ttf-jetbrains-mono-nerd tesseract-data-eng \
-  vlc obs-studio zen-browser-bin brave-bin \
-  vesktop telegram-desktop steam \
-  btop tree npm
+	hyprland kitty fish neovim git ufw \
+	ttf-jetbrains-mono-nerd tesseract-data-eng \
+	vlc obs-studio zen-browser-bin brave-bin \
+	vesktop telegram-desktop steam \
+	btop tree npm paru yay fuse2
